a0d438d255d4f4697ace97642d88f3ea4c8626ac
Khalid <khalid@Macshops-JNFVTKW9CQ.local>
2026-07-02 17:53:00 +0500

    nimadir

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import car from "./assets/img/car.webp";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Salom bu mening birinchi react app im</h1>
+
+      <h1>Mening ismim : {"name"}</h1>
+
+      <img width={"200"} src={car} alt="rasm " />
+    </div>
+  );
+};
+
+export default App;
